728243a5c8db4f6f305d79ea8a0cf7b34c24cfee
marcelo janio <marcelostgm@gmail.com>
2022-07-31 15:24:26 -0300

    close issue #4

---
+++ ContaCorrente.java
@@ -0,0 +1,8 @@
+public class ContaCorrente extends Conta {
+
+    
+    public ContaCorrente(int numeroDaConta, int agencia, Cliente cliente) {
+        super(numeroDaConta, agencia, cliente);
+    }
+    
+}
