9dd31bb71534c96af1bfba3cfb083d3fd91c9523
Victor <victorribeiro172@gmail.com>
2022-07-31 14:51:34 -0300

    Criar Cliente #1

---
+++ Cliente.java
@@ -0,0 +1,37 @@
+public class Cliente {
+
+    private String nome;
+    private int cpf;
+    private String sexo;
+    
+    public Cliente(String nome, int cpf, String sexo){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+}
